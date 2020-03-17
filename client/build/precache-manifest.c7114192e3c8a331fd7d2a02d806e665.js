b5c7a753b7f35bcf2c324129a56687572da38764
Fisayo <aikomofisayo@gmail.com>
2020-03-17 02:30:48 +0100

    Build and test for production

---
+++ client/build/precache-manifest.c7114192e3c8a331fd7d2a02d806e665.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e84fc2f7eec1ac2147dd167315cbe97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1223baf0ae2d0de4128d",
+    "url": "/static/css/main.016ee906.chunk.css"
+  },
+  {
+    "revision": "2f5bedbb1a9d9d3d5724",
+    "url": "/static/js/2.de8aca29.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.de8aca29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1223baf0ae2d0de4128d",
+    "url": "/static/js/main.bb66b9ec.chunk.js"
+  },
+  {
+    "revision": "c4ed5584fc59eb1efa3b",
+    "url": "/static/js/runtime-main.93cce1dd.js"
+  }
+]);
